d568d5631b264fc2782aa743326983b02fb8ae63
S A M <119486233+hamzamamdouh298@users.noreply.github.com>
2024-11-23 09:22:17 +0200

    Update app.js

--- LASTUPDATE/app.js
+++ LASTUPDATE/app.js
@@ -2,7 +2,7 @@ const menu = document.querySelector('#mobile-menu');
 const menuLinks = document.querySelector('.navbar__menu');
 const navLogo = document.querySelector('#navbar__logo');
 
-// Display Mobile Menu
+
 const mobileMenu = () => {
   menu.classList.toggle('is-active');
   menuLinks.classList.toggle('active');
